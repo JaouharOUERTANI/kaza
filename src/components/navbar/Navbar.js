7b2bf6563e2f1643bd3ae421fe1bcd14fa254a95
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-09-04 15:32:49 +0200

    🐛 E10: fix: Resolve server launch issue

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,23 @@
-import './navbar.scss'
-import { Link } from 'react-router-dom'
+import './navbar.scss';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Navbar() {
-
-    const currentRoute = window.location.pathname;
+    const location = useLocation();
 
     return (
         <nav className='nav'>
             <ul className='nav_list'>
-                <li className={currentRoute === '/Kasa/' ? 'nav_list_item_active' : 'nav_list_item'}>
-                    <Link  to='/Kasa/'>
+                <li className={location.pathname === '/' ? 'nav_list_item_active' : 'nav_list_item'}>
+                    <Link to='/'>
                         Accueil
                     </Link>
                 </li>
-                <li className={currentRoute === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
-                    <Link  to='/about'>
-                        A propos
+                <li className={location.pathname === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
+                    <Link to='/about'>
+                        À propos
                     </Link>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
