9532cc832bda9df621db9304113595096adf1d9c
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-09-05 16:08:57 +0200

    🐛 E09: feat: Add error page redirection for invalid IDs

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Les règles ESLint vont ici
+    rules: {
+      "no-console": "off", // Exemple de règle personnalisée
+    },
+    globals: {
+        Navigate: 'readonly'
+      },
+  };
+  
